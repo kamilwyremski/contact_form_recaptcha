a1c6b3e131faa30b65b026a133610c3ae3906471
Kamil Wyremski <kamil.wyremski@gmail.com>
2020-07-11 09:43:08 -0500

    Obsługa reCATPCHA v3
    Aktualizacja skryptu do obsługi reCATPCHA w wersji v3

--- contact.php
+++ contact.php
@@ -6,8 +6,17 @@ session_start();
 
 if(!empty($_POST['name']) and !empty($_POST['email']) and !empty($_POST['message'])){
 	
-	$response=json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=<SECRET_KEY>&response=".$_POST['g-recaptcha-response']."&remoteip=".$_SERVER['REMOTE_ADDR']), true);
-	
+	$verify = curl_init();
+	curl_setopt($verify, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
+	curl_setopt($verify, CURLOPT_POST, true);
+	curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query([
+		'secret' => '<SECRET_KEY> ',
+		'response' => $_POST['recaptcha_response']
+	]));
+	curl_setopt($verify, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
+	$response = json_decode(curl_exec($verify),true);
+
     if($response['success'] == false){
 		die('Kod captcha jest nieprawidłowy');
 		$input = $_POST;
@@ -42,21 +51,11 @@ if(!empty($_POST['name']) and !empty($_POST['email']) and !empty($_POST['message
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>Formularz kontaktowy</title>
 	<link rel="stylesheet" href="style.css">
-	<script>
-		var onloadCaptcha = function() {
-			grecaptcha.render('g-recaptcha', {
-			  'sitekey' : '<SITE_KEY>',
-			  'callback' : function() {
-				document.getElementById('submit').disabled = false;
-			  }
-			});
-		};
-	</script>
-	<script src='https://www.google.com/recaptcha/api.js?onload=onloadCaptcha'></script>
 </head>
 <body>
 
 <form method="post">
+	<input type="hidden" name="recaptcha_response" class="recaptchaResponse">
     <label for="name">Imię i nazwisko</label>
     <input type="text" name="name" id="name" placeholder="Jan Kowalski" required>
 
@@ -65,11 +64,20 @@ if(!empty($_POST['name']) and !empty($_POST['email']) and !empty($_POST['message
 
     <label for="message">Wiadomość</label>
     <textarea name="message" id="message" placeholder="Wpisz swoją wiadomość" required></textarea>
-	
-	<div id="g-recaptcha"></div>
-
-    <input type="submit" name="submit" value="Wyślij" disabled id="submit">
+    <input type="submit" name="submit" value="Wyślij">
 </form>
 
+<script src="https://www.google.com/recaptcha/api.js?render=<SITE_KEY>"></script>
+<script>
+	grecaptcha.ready(function () {
+		grecaptcha.execute('<SITE_KEY>', { action: 'contact' }).then(function (token) {
+			var elms = document.getElementsByClassName('recaptchaResponse')
+			for (var i = 0; i < elms.length; i++) {
+				elms[i].setAttribute("value", token);
+			}
+		});
+	});
+</script>
+	
 </body>
 </html>
